0b0a67d94f015fd548b55e295e79b667c41ae431
hinx <b2765270@gmail.com>
2017-03-31 23:21:07 +0800

    update

--- build/dev-server.js
+++ build/dev-server.js
@@ -3,40 +3,30 @@ const config = require('../config')
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
+const port = process.env.PORT || config.dev.port
 
 const Koa = require('koa')
 const Router = require('koa-router')
 const serve = require('koa-static')
-const mountHtml = require('koa-mount-html')
+const rewrite = require('koa-rewrite')
 // var session = require('koa-session')
 const path = require('path')
 const middleware = require('koa-webpack')
 const webpack = require('webpack')
 const webpackConfig = require('./webpack.dev.conf')
-// const webpackDevMiddleware = require('webpack-dev-middleware')
-const mount = require('koa-mount');
-
-// default port where dev server listens for incoming traffic
-const port = process.env.PORT || config.dev.port
 
 const app = new Koa();
 const router = new Router();
 const compiler = webpack(webpackConfig)
-const publicPath = webpackConfig.output.publicPath
 
-router.get('/a', (ctx, next) => {
-  
+router.get('/api', async (ctx, next) => {
+  await next()
 })
 
-// app.use(mountHtml(async (ctx, next) => {
-//   await send(ctx, '/index.html', { root: __dirname + '/static' });
-// }, { defer: true }))
-
-// app.use(mountHtml(
-//   publicPath, webpackDevMiddleware(compiler)
-// ))
-
 app
+  .use(
+    rewrite('/test', '/')
+  )
   .use(router.routes())
   .use(router.allowedMethods())
   .use(serve('static'))
@@ -46,4 +36,4 @@ app
 
 app.listen(port)
 
-console.log(`http://localhost:${port}`)
+console.log(`Listening on: http://localhost:${port}`)
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>我我我3</h2>
+    <h2>Vue</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">你好</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
